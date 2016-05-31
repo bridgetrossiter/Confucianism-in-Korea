9cd95c938971ada8eaff359015b756f52fb02d31
rosbe002 <rosbe002@mymail.unisa.edu.au>
2016-05-31 20:25:53 +0900

    Unfinished Linking Method

---
+++ Confucianism in Korea/MovieViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MovieViewController.swift
+//  Confucianism in Korea
+//
+//  Created by Bridget Rossiter on 31/05/2016.
+//  Copyright © 2016 Bridget Rossiter. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class MovieViewController: UIViewController {
+    
+    var player: AVPlayer? = nil
+
+    @IBOutlet var playerView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let videoURL = NSURL(string: "http://www.filedropper.com/castielreturnssupernaturalfanmadepromo.mp4")
+        player = AVPlayer(URL: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.playerView.bounds
+        self.playerView.layer.addSublayer(playerLayer)
+        player!.play()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        player!.play()
+    }
+
+    @IBAction func pause(sender: AnyObject) {
+        player!.pause()
+    }
+    @IBAction func rewind(sender: AnyObject) {
+        player!.pause()
+        player!.seekToTime(kCMTimeZero)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
